48e459f82a84d7ece9e180363449b72c17dd9c2c
Matthew <matthew.k913@gmail.com>
2017-12-05 22:15:20 -0500

    added updated code

--- Question2/GeneralN.m
+++ Question2/GeneralN.m
@@ -1,7 +1,7 @@
 close all
 clear variables
 clc
-N= 5 %This is the variable we want to change
+N= 40 %This is the variable we want to change
 M=(N-1)^2
 L= N-1
  
@@ -9,7 +9,7 @@ A=zeros(M,M)
 
 %Below is the code for a general A matrix%
 A(1:M+1:end)=-4 
-A(N:M+1:end)=1
+A(N:M+1:(M+1)*(M-L))=1
 A(2:M+1:end)=1
 A(2+(M+1)*(L-1) :(M+1)*L:end) = 0
 A(M+1:M+1: end) = 1
@@ -21,16 +21,33 @@ A((M+1)+(M*1)*(N-2):M+1:end)=1
 %Assign rows and columns
 B=[1:M]
 B(1:M)=0
-B(1:1:L) = 90
+B(1:1:L) = -90
 B(1:L:end) = 0
-B((L)*(L-1)+1:1:end) = 30
-B(L:L:end) = 60 
+B((L)*(L-1)+1:1:end) = -30
+B(L:L:end) = -60 
 
 %Assign Corners
-B(1) = 90
-B(L) = 150
-B(L^2) = 90
-B((L^2)-(L-1)) = 30
+B(1) = -90
+B(L) = -150
+B(L^2) = -90
+B((L^2)-(L-1)) = -30
 
-u=A\B
+C = transpose(B)
+
+u=inv(A)*C
+
+p=reshape(u,L,L)
+
+NewMatrix = zeros(L+2,L+2)
+NewMatrix(2:(L+1),2:(L+1))=p
+
+NewMatrix(1:L+2,L+2)=30
+NewMatrix(L+2,1:L+2)=60
+NewMatrix(1:L+2,1)=90
+
+New = transpose(NewMatrix)
+
+surf(New)
+colormap(cool)
+
 
